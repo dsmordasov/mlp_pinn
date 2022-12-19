ab8a49212a7d8fb74c69612cf891eab6032f71b1
Dmitrij <d.s.mordasov@gmail.com>
2022-12-19 17:50:41 +0100

    complete tasks 1.1-1.4

--- DT_and_NN_Security_Assessment_14bus.py
+++ DT_and_NN_Security_Assessment_14bus.py
@@ -1,14 +1,17 @@
+# DISCLAIMER: Most of this code is provided as an assignment, please do not 
+#             attribute the lack of coding style conventions to me (Dmitrij)
+
 # all these modules are necessary
-import numpy
+import numpy # as np, this hurts, who would just do this
 from sklearn import tree
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
 # insert the directory where the data is stored
-data_directory = ""
+data_directory = "D:/git/mlp_pinn/"
 
-# COMMENT
+# COMMENT - loads X, OPF database without VG
 print("Loading generator set-points -- features for machine learning")
 # ATTENTION: The MAT files need to be converted to CSV files to be imported (see MATLAB fct for that)
 generator_dispatch = numpy.loadtxt(open(data_directory + "generator_dispatch.csv", "rb"), delimiter=",", skiprows=0)
@@ -16,27 +19,30 @@ print(generator_dispatch)
 print(generator_dispatch.shape)
 # normalize features for better performance
 
-# COMMENT
-print("Loading  security classification N-1 security")
+# COMMENT - loads y
+print("Loading security classification N-1 security")
 classification_N1 = numpy.loadtxt(open(data_directory + "classification_N1.csv", "rb"), delimiter=",", skiprows=0)
-# COMMENT
+# COMMENT - one hot encode (OHE) the binary classification y vector (for improved NN operation), y_1 = 1 safe, y_2 = 2 unsafe
 classification_N1 = numpy.row_stack(([classification_N1], [numpy.ones(classification_N1.shape) - classification_N1]))
 print(classification_N1)
-print("The number of secure samples:", numpy.sum(classification_N1))
+print(classification_N1.shape)
+print("The number of secure samples:", numpy.sum(classification_N1[0])) # 885
+# assignment assumed to be missing the [0], otherwise it just gives you the number of cases due to OHE
+# not following the database convention of examples stored in rows, this hurts 
 
-
-# COMMENT
+# COMMENT - loads y, with a different (more strict) classification criterion
 print("security classification N-1 security and small-signal stability")
 classification_N1_SSS = numpy.loadtxt(open(data_directory + "classification_N1_SSS.csv", "rb"), delimiter=",",
                                       skiprows=0)
-# COMMENT
+# COMMENT - OHE the binary classification y vector 
 classification_N1_SSS = numpy.row_stack(
     ([classification_N1_SSS], [numpy.ones(classification_N1_SSS.shape) - classification_N1_SSS]))
 print(classification_N1_SSS)
 print(numpy.sum(classification_N1))
-print("The number of secure samples:", numpy.sum(classification_N1_SSS))
+print("The number of secure samples:", numpy.sum(classification_N1_SSS[0])) # 218
+# assignment assumed to be missing the [0], otherwise it just gives you the number of cases due to OHE
 
-# COMMENT
+# COMMENT - shuffle the datasets, split into training/test (0.8/0.2) sets
 print(classification_N1.shape[0])
 indices = numpy.random.permutation(generator_dispatch.shape[1])
 print(indices)
@@ -60,7 +66,7 @@ classification_N1_SSS_test = classification_N1_SSS[:, test_idx]
 # QUESTION: For which of the two databases are we training the DT and NN? 
 #           Why don't you try to train a DT and an NN for the other database too? 
 print("-------------------------------------------")
-print("train a decision tree and compute accuracy")
+print("Train a decision tree and compute accuracy")
 
 # simple decision tree training
 # from webpage https://scikit-learn.org/stable/modules/tree.html
@@ -72,32 +78,35 @@ print("train a decision tree and compute accuracy")
 # clf = clf.fit(X, Y)
 # tree.plot_tree(clf)
 
-#COMMENT
+#COMMENT - define a decision tree classifier model, and train it on training data (the less-tight criterion one)
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(numpy.transpose(generator_dispatch_train), classification_N1_train[0, :])
 # plot decision tree
-# tree.plot_tree(clf)
+tree.plot_tree(clf)
 
 
-# COMMENT
+# COMMENT - run the trained model on test (and training?) data
 classification_N1_test_pred = clf.predict(numpy.transpose(generator_dispatch_test))
 classification_N1_train_pred = clf.predict(numpy.transpose(generator_dispatch_train))
 
-# COMMENT
+# COMMENT - calculate the accuracy performance metric of the model on the training dataset
 acc_train = 100 * (1 - numpy.sum(numpy.absolute(classification_N1_train_pred - classification_N1_train[0, :])) / (
     len(classification_N1_train[0, :])))
 
-print("Decision tree training accuracy", acc_train)
-# COMMENT
+print(f"Decision tree training accuracy: {acc_train} %")
+# 100% accuracy was not unexpected for a decision tree model
+# COMMENT - calculate the accuracy performance metric of the model, now on yet unseen test data
 acc_test = 100 * (1 - numpy.sum(numpy.absolute(classification_N1_test_pred - classification_N1_test[0, :])) / (
     len(classification_N1_train[0, :])))
-print("Decision tree test accuracy", acc_test)
+print(f"Decision tree test accuracy {round(acc_test, 5)} %")
+# Quite 'accurate', but as discussed, a different metric should likely be used in this case
+# 
 
-# COMMENT
+# COMMENT 
 print("-------------------------------------------")
-print("train a neural network and compute accuracy")
+print("Train a neural network and compute accuracy")
 
-# COMMENT
+# COMMENT - NN model with 3 hidden layers, 20 nodes each, relu activation function, and for output layer spit out probabilities y_1 and y_2 of whether the system is safe or unsafe
 NN = keras.Sequential([
     layers.Dense(20, activation='relu', input_shape=[5]),
     layers.Dense(20, activation='relu'),
@@ -105,12 +114,12 @@ NN = keras.Sequential([
     layers.Dense(2, activation='softmax')
 ])
 
-#COMMENT
+#COMMENT - implement Adam algorithm for backpropagation, a cheap gradient descent method
 optimizer = tf.keras.optimizers.Adam(0.001)
 # there are a lot of options for the optimizer
 # tf.keras.optimizers.RMSprop(0.001)
 
-# COMMENT
+# COMMENT - compile the model with the defined NN, using the binary cross-entropy function, and the above-defined optimiser
 NN.compile(loss='binary_crossentropy',
            optimizer=optimizer)
 
@@ -119,16 +128,16 @@ history = NN.fit(numpy.transpose(generator_dispatch_train), numpy.transpose(clas
                  validation_data=(numpy.transpose(generator_dispatch_test), numpy.transpose(classification_N1_test)),
                  batch_size=500)
 
-# COMMENT
+# COMMENT - see what the model predicts on the training (sanity check) and test datasets
 classification_N1_test_pred = NN.predict(numpy.transpose(generator_dispatch_test))
 classification_N1_train_pred = NN.predict(numpy.transpose(generator_dispatch_train))
 
-# COMMENT
+# COMMENT - NN training model accuracy not expected to be 100% like with a decision tree
 acc_train = 100 * (1 - numpy.sum(numpy.absolute(classification_N1_train_pred[:, 0] - classification_N1_train[0, :])) / (
     len(classification_N1_train[0, :])))
 
 print("Neural network training accuracy", acc_train)
-# COMMENT
+# COMMENT - the NN ends up being more accurate on the test set rather than the data set, explainable by data set sizes and NN behaviour
 acc_test = 100 * (1 - numpy.sum(numpy.absolute(classification_N1_test_pred[:, 0] - classification_N1_test[0, :])) / (
     len(classification_N1_train[0, :])))
 print("Neural network test accuracy", acc_test)
